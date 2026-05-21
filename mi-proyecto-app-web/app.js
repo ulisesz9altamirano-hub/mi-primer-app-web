6a1e4495c175540d5231b000460277035d1cf700
Ulisesaltamirano <ulises.z9.altamirano@gmail.com>
2026-05-21 14:21:49 -0300

    Refactor login modal functions and processing

--- mi-proyecto-app-web/app.js
+++ mi-proyecto-app-web/app.js
@@ -1,4 +1,4 @@
-// Funciones para manejar el cartel flotante (Modal) de Login
+
 function mostrarModal() {
     document.getElementById('login-modal').classList.remove('hidden');
 }
@@ -7,16 +7,16 @@ function cerrarModal() {
     document.getElementById('login-modal').classList.add('hidden');
 }
 
-// Función para simular el inicio de sesión
+
 function procesarLogin(event) {
-    event.preventDefault(); // Evita que la página se recargue
+    event.preventDefault(); 
     
     const email = document.getElementById('email').value;
     
-    // Simulamos que el login fue exitoso
+  
     cerrarModal();
     
-    // Cambiamos los botones de la barra de navegación
+    
     document.getElementById('btn-login-nav').classList.add('hidden');
     
     const userBadge = document.getElementById('user-badge');
